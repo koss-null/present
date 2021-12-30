e61fa011109878c61db6e7965a5bc0e11d933080
Dmitry Kossovich <d.kossovich@tinkoff.ru>
2021-12-30 05:23:25 +0400

    some result finally

--- .secret/main.js
+++ .secret/main.js
@@ -28,19 +28,37 @@
         letter.classList.add("in");
     }
 
+    var flag = 1;
+
     function closeEnvelopeOnHover() {
-        var flag = 1;
         if (flag === 1) {
             putIn();
             envelopeTop.classList.remove("open");
-            envelopeTop.classList.add("closed");
+            envelopeTop.classList.add("close");
         } else {
-            envelope.removeEventListener('mouseout', closeEnvelopeHover);
+            envelope.removeEventListener('mouseout', closeEnvelopeOnHover);
         }
     }
 
     function openEnvelope() {
-
+        flag = 0;
+        closeEnvelopeOnHover();
+        letter.classList.add("pull");
+        letter.addEventListener("animationend", function() {
+            left.style.zIndex = 0;
+            bottomRight.style.zIndex = 0;
+            envelopeTop.style.zIndex = 0;
+            letter.style.zIndex = 20;
+            letter.classList.add("put");
+            letter.addEventListener("animationend", function() {
+                letter.style.transition = "none";
+                letter.classList.add("final");
+                letter.classList.remove("put");
+                letter.classList.remove("pull");
+                envelope.style.cursor = "default";
+            });
+        });
+        envelope.removeEventListener('click', openEnvelope);
     }
 
 
